4861d17fa4c225b63bb7bb9394280afd82a037c6
dimitricaloz <dimitri.caloz@studentfr.ch>
2023-06-19 11:19:06 +0200

    program
    abandonné pour le moment

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,23 @@
-// const containerVendredi = document.querySelector(".container-vendredi");
-// const buttonVendredi = document.querySelector(".button-vendredi");
-// var openMenu = function () {
-//   containerVendredi.classList.toggle("is-open");
-// };
+const containerVendredi = document.querySelector(".container-vendredi");
+const buttonVendredi = document.querySelector(".button-vendredi");
+var openMenu = function () {
+  containerVendredi.classList.toggle("is-open");
+};
 
-// buttonVendredi.addEventListener("click", openMenu);
+buttonVendredi.addEventListener("click", openMenu);
 
-// const containerSamedi = document.querySelector(".container-samedi");
-// const buttonSamedi = document.querySelector(".button-samedi");
-// var openMenu = function () {
-//   containerSamedi.classList.toggle("is-open");
-// };
+const containerSamedi = document.querySelector(".container-samedi");
+const buttonSamedi = document.querySelector(".button-samedi");
+var openMenu = function () {
+  containerSamedi.classList.toggle("is-open");
+};
 
-// buttonSamedi.addEventListener("click", openMenu);
+buttonSamedi.addEventListener("click", openMenu);
 
-// const containerDimanche = document.querySelector(".container-dimanche");
-// const buttonDimanche = document.querySelector(".button-dimanche");
-// var openMenu = function () {
-//   containerDimanche.classList.toggle("is-open");
-// };
+const containerDimanche = document.querySelector(".container-dimanche");
+const buttonDimanche = document.querySelector(".button-dimanche");
+var openMenu = function () {
+  containerDimanche.classList.toggle("is-open");
+};
 
-// buttonDimanche.addEventListener("click", openMenu);
+buttonDimanche.addEventListener("click", openMenu);
