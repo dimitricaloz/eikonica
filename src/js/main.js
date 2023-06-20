444754bd58a0f028a9d4fc3689e207ad7656341e
dimitricaloz <dimitri.caloz@studentfr.ch>
2023-06-20 17:17:02 +0200

    fin html css et java

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ const containerVendredi = document.querySelector(".container-vendredi");
 const buttonVendredi = document.querySelector(".button-vendredi");
 var openMenu = function () {
   containerVendredi.classList.toggle("is-open");
+  buttonVendredi.classList.toggle("is-active");
 };
 
 buttonVendredi.addEventListener("click", openMenu);
@@ -10,6 +11,7 @@ const containerSamedi = document.querySelector(".container-samedi");
 const buttonSamedi = document.querySelector(".button-samedi");
 var openMenu = function () {
   containerSamedi.classList.toggle("is-open");
+  buttonSamedi.classList.toggle("is-active");
 };
 
 buttonSamedi.addEventListener("click", openMenu);
@@ -18,6 +20,19 @@ const containerDimanche = document.querySelector(".container-dimanche");
 const buttonDimanche = document.querySelector(".button-dimanche");
 var openMenu = function () {
   containerDimanche.classList.toggle("is-open-last-one");
+  buttonDimanche.classList.toggle("is-active");
 };
 
 buttonDimanche.addEventListener("click", openMenu);
+
+const languageSelectors = document.querySelectorAll(".language-selector");
+
+languageSelectors.forEach((selector) => {
+  selector.addEventListener("click", function () {
+    languageSelectors.forEach((selector) => {
+      selector.classList.remove("active");
+    });
+
+    this.classList.add("active");
+  });
+});
